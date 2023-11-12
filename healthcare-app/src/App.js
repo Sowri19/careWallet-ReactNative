d04418c1356e81bd6c61df4a1f0ab720f0ae4ad5
GORLEABHILASH <36183586+GORLEABHILASH@users.noreply.github.com>
2023-11-11 19:06:47 -0500

    Added Visits Code

---
+++ healthcare-app/src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import PatientList from './components/PatientList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Healthcare App</h1>
+      <PatientList />
+    
+    </div>
+  );
+}
+
+export default App;
