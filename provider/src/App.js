786a336dacbe162810d68d33f265e501f5f97bea
Allen Lin <allenlinsh@gmail.com>
2023-12-02 04:31:51 -0500

    Added react-cookies

--- provider/src/App.js
+++ provider/src/App.js
@@ -1,16 +1,27 @@
 // src/App.js
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from './components/Login';
 import PatientList from './components/PatientList';
+import { CookiesProvider, useCookies } from "react-cookie";
 
 const App = () => {
   const [loggedInDoctor, setLoggedInDoctor] = useState(null);
+  const [cookies, setCookie] = useCookies(["doctor"]);
 
   const handleLogin = (doctor) => {
     setLoggedInDoctor(doctor);
+    setCookie("doctor", doctor, { path: "/" });
   };
 
+  useEffect(() => {
+    const doctor = cookies.doctor
+    if (doctor) {
+      setLoggedInDoctor(doctor);
+    }
+  }, [cookies.doctor])
+
   return (
+    <CookiesProvider>
     <div>
       {!loggedInDoctor ? (
         <Login onLogin={handleLogin} />
@@ -18,6 +29,7 @@ const App = () => {
         <PatientList loggedInDoctor={loggedInDoctor} />
       )}
     </div>
+    </CookiesProvider>
   );
 };
 
