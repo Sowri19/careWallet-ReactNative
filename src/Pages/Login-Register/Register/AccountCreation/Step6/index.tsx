2e4372d5e2dd403c93c803284ed2905de8c58182
shashibyadav <shashi.shuttle2@gmail.com>
2024-01-29 23:04:59 -0500

    camera fix

--- src/Pages/Login-Register/Register/AccountCreation/Step6/index.tsx
+++ src/Pages/Login-Register/Register/AccountCreation/Step6/index.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IDScanner from '../../../../../Components/Camera/index';
 import { PagesProps } from '../../../../../utilities/CommonTypes';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../../../ReduxStore/Setup/store';
+import { useFocusEffect } from '@react-navigation/native';
+import { getCameraPermission } from '../../../../../utilities/commonUtilFunctions';
+import { setCameraPermission } from '../../../../../ReduxStore/Slices/CameraSlice/CameraSlice';
 
 const IDFront: React.FC<PagesProps> = ({ navigation }) => {
+  const cameraPermission = useSelector(
+    (state: RootState) => state.camera.hasCameraPermission
+  );
+  const dispatch = useDispatch();
+  useFocusEffect(
+    React.useCallback(() => {
+      setTimeout(async () => {
+        if (!cameraPermission) {
+          const cameraStatus = await getCameraPermission();
+          dispatch(setCameraPermission(cameraStatus.status === 'granted'));
+        }
+      });
+      return () => {};
+    }, [])
+  );
   return (
     <IDScanner
       navigation={navigation}
